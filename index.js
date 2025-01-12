bd252249a130a99a85395b1afe79017a7da07db7
ashraf <ashraf.osman3000@gmail.com>
2025-01-12 16:21:33 -0500

    port change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', function (req, res) {
   res.send('Hello World from ' + req.protocol + '://' + req.get('host') + req.originalUrl+'\n*****Version: 0.0.1*****');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(8080, function () {
   var port = server.address().port;
 
   console.log('Your nodejs app is listening at port %s',  port);
