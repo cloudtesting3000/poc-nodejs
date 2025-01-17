d0ab6aba4e56ef5bb8e1509a426dcd8070b73faf
ashraf <ashraf.osman3000@gmail.com>
2025-01-17 15:50:12 -0500

    var change

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+const OCP = process.env.OCP || 'Var. error';
+
 app.get('/', function (req, res) {
-  res.send('Hello World from ' + req.protocol + '://' + req.get('host') + req.originalUrl+'\n*****Version: 0.0.1*****');
+  res.send('Hello World from ' + OCP + '\n' + req.protocol + '://' + req.get('host') + req.originalUrl);
 });
 
 var server = app.listen(8080, function () {
   var port = server.address().port;
 
-  console.log('Your nodejs app is listening at port %s',  port);
+  console.log('nodejs app is listening at port %s',  port);
 });
