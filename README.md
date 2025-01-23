c93bf91afd9e0b02d682b408d0a8d1520cbcf1fd
ashraf <ashraf.osman3000@gmail.com>
2025-01-22 22:20:28 -0500

    Message formatting

--- README.md
+++
@@ -1,3 +0,0 @@
-# simple-nodejs-app
-
-This is a simple nodejs app that prints out Hello World echoing the absolute URL of the server it runs on and a static version #.
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 
-const OCP = process.env.OCP || 'Var. error';
+const OCP = process.env.OCP || 'variable not found error';
 
 app.get('/', function (req, res) {
-  res.send('Hello World from ' + OCP + '\n' + req.protocol + '://' + req.get('host') + req.originalUrl);
+  res.send(`Hello from: ${OCP} <br> Your request: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
 });
 
 var server = app.listen(3000, function () {
-  var port = server.address().port;
+var port = server.address().port;
 
-  console.log('nodejs app is listening at port %s',  port);
+console.log('nodejs app is listening on port %s',  port);
 });
